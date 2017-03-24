3219fd6bf26bd628d1ec16e4ca2751fcbf00c4ff
Dominik Gresch <greschd@gmx.ch>
2017-03-24 11:48:27 +0100

    Add draft for parsing plugin

---
+++ aiida/parsers.plugins.tbmodels/tbmodels.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Author:  Dominik Gresch <greschd@gmx.ch>
+
+import json
+
+from aiida.parsers.parser import Parser
+from aiida.orm.data.singlefiledata import SinglefileData
+
+class TbmodelsParser(Parser):
+    """
+    Parse TBmodels output to a SinglefileData containing the model file.
+    """
+    def parse_with_retrieved(self, retrieved):
+        try:
+            out_folder = retrieved[self._calc._get_linkname_retrieved()]
+        except KeyError:
+            self.logger.error("No retrieved folder found")
+
+        model_file = out_folder.get_abs_path(self._calc._OUTPUT_FILE_NAME)
+        model_node = DataFactory('singlefile')()
+        model_node.add_path(model_file)
+        new_nodes_list = [('tb_model', model_node)]
+
+        return True, new_nodes_list
