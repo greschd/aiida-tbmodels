06d38efabcc405d96c6b4aed9550bef24a24d0c5
Dominik Gresch <greschd@gmx.ch>
2017-03-30 18:32:53 +0200

    Add tb model in run.py

--- examples/slice/run.py
+++ examples/slice/run.py
@@ -10,10 +10,35 @@ import itertools
 
 from aiida.orm import DataFactory, Code, CalculationFactory, Computer, QueryBuilder
 
+def get_singlefile_instance(description, path):
+    qb = QueryBuilder()
+    qb.append(
+        SinglefileData,
+        filters={'description': {'==': description}}
+    )
+    res = qb.all()
+    if len(res) == 0:
+        # create archive
+        res = SinglefileData()
+        res.add_path(os.path.abspath(path))
+        res.description = description
+        res.store()
+    elif len(res) > 1:
+        raise ValueError('Query returned more than one matching SinglefileData instance.')
+    else:
+        res = res[0][0]
+    return res
+
 def run_slice():
     code = Code.get_from_string('tbmodels_dev')
     calc = CalculationFactory('tbmodels.slice')()
     calc.use_code(code)
+
+    calc.use_tb_model(get_singlefile_instance(
+        description=u'InSb TB model',
+        path='./reference_input/model.hdf5'
+    ))
+
     # single-core on local machine
     calc.set_resources(dict(
         num_machines=1,
