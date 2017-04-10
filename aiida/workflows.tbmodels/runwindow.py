dbc286143038a1349e06c9f760135718123d8c89
Dominik Gresch <greschd@gmx.ch>
2017-04-10 11:51:18 +0200

    Add example for runwindow

--- aiida/workflows.tbmodels/runwindow.py
+++ aiida/workflows.tbmodels/runwindow.py
@@ -8,124 +8,49 @@ from aiida_tools.validate_input import validate_input, parameter, inherit_parame
 from aiida.orm import (
     Code, Computer, DataFactory, CalculationFactory, QueryBuilder, Workflow, WorkflowFactory
 )
+from .bandevaluation import BandevaluationWorkflow
+from .tbextraction import TbextractionWorkflow
 
-@inherit_parameters(WorkflowFactory('tbmodels.bandevaluation'))
-@inherit_parameters(WorkflowFactory('tbmodels.tbextraction'))
+@validate_input
+@parameter('window', DataFactory('parameter'))
+@inherit_parameters(BandevaluationWorkflow)
+@inherit_parameters(TbextractionWorkflow)
 class RunwindowWorkflow(Workflow):
     """
-    This workflow takes a runs the tight-binding extraction and analysis for a given energy window.
+    This workflow runs the tight-binding extraction and analysis for a given energy window.
     """
     def __init__(self, **kwargs):
         super(RunwindowWorkflow, self).__init__(**kwargs)
 
+    @Workflow.step
+    def start(self):
+        extraction_params = self.inherited_parameters(TbextractionWorkflow)
+        # set the energy window
+        wannier_settings = extraction_params['wannier_settings'].get_dict()
+        wannier_settings.update(self.get_parameter('window').get_dict())
+        wannier_settings_data = DataFactory('parameter')(dict=wannier_settings)
+        wannier_settings_data.store()
+        extraction_params['wannier_settings'] = wannier_settings_data
+        wf = TbextractionWorkflow(params=extraction_params)
+        wf.store_all()
+        self.attach_workflow(wf)
+        self.next(self.bandeval)
 
-#     @property
-#     def previous_step(self):
-#         return eval('self.' + self.get_attribute('steps_done')[-1])
-#
-#     def get_next_step(self):
-#         # This is a bit of a hack -- but it enables chaining the steps without
-#         # having to write lots of logic to detect which was the previous step
-#         # For it to work, get_next_step must be used in the self.next call.
-#         steps_todo = self.get_attribute('steps_todo')
-#         steps_done = self.get_attribute('steps_done')
-#         try:
-#             current_step = self.get_attribute('current_step')
-#             steps_done += [current_step]
-#         except ValueError:
-#             pass
-#
-#         current_step = steps_todo.pop(0)
-#         self.add_attribute('current_step', current_step)
-#         self.add_attribute('steps_todo', steps_todo)
-#         self.add_attribute('steps_done', steps_done)
-#         return eval('self.' + current_step)
-#
-#     def run_wswannier(self):
-#         input_archive = self.get_parameter('wannier_data')
-#         calc = CalculationFactory('vasp.wswannier')()
-#         code = Code.get_from_string(self.get_parameter('wannier_code'))
-#         calc.use_code(code)
-#         # No MPI
-#         calc.set_resources(dict(num_machines=1, tot_num_mpiprocs=1))
-#         calc.set_computer(code.get_computer())
-#         calc.set_queue_name(self.get_parameter('wannier_queue'))
-#         calc.use_data(input_archive)
-#         # set default for write_tb etc.
-#         wannier_settings = self.get_parameter('wannier_settings').get_dict()
-#         wannier_settings.setdefault('write_hr', True)
-#         # wannier_settings.setdefault('write_xyz', True)
-#         wannier_settings.setdefault('use_ws_distance', True)
-#         # TODO: caching!
-#         calc.use_settings(DataFactory('parameter')(dict=wannier_settings))
-#         calc.store_all()
-#         return calc
-#
-#     @Workflow.step
-#     def start(self):
-#         self.validate_input()
-#         self.append_to_report("Running Wannier90 calculation...")
-#         self.attach_calculation(self.run_wswannier())
-#         self.next(self.get_next_step())
-#
-#     def setup_tbmodels(self, calc):
-#         code = Code.get_from_string(self.get_parameter('tbmodels_code'))
-#         calc.use_code(code)
-#         calc.set_resources({'num_machines': 1})
-#         calc.set_withmpi(False)
-#         calc.set_computer(code.get_computer())
-#
-#     def run_parse(self, wannier_folder):
-#         calc = CalculationFactory('tbmodels.parse')()
-#         self.setup_tbmodels(calc)
-#
-#         calc.use_wannier_folder(wannier_folder)
-#         calc.store_all()
-#         return calc
-#
-#     @Workflow.step
-#     def parse(self):
-#         wannier_calc = self.get_step_calculations(self.start)[0]
-#         wannier_folder = wannier_calc.out.tb_model
-#         self.append_to_report("Parsing Wannier90 output to tbmodels format...")
-#         self.attach_calculation(self.run_parse(wannier_folder))
-#         self.next(self.get_next_step())
-#
-#     def run_slice(self, tbmodel_file):
-#         calc = CalculationFactory('tbmodels.slice')()
-#         self.setup_tbmodels(calc)
-#         calc.use_tb_model(tbmodel_file)
-#         calc.use_slice_idx(self.get_parameter("slice_idx"))
-#         calc.store_all()
-#         return calc
-#
-#     @Workflow.step
-#     def slice(self):
-#         calc = self.get_step_calculations(self.previous_step)[0]
-#         tbmodel_file = calc.out.tb_model
-#         self.append_to_report("Slicing tight-binding model...")
-#         self.attach_calculation(self.run_slice(tbmodel_file))
-#         self.next(self.get_next_step())
-#
-#     def run_symmetrize(self, tbmodel_file):
-#         calc = CalculationFactory('tbmodels.symmetrize')()
-#         self.setup_tbmodels(calc)
-#         calc.use_tb_model(tbmodel_file)
-#         calc.use_symmetries(self.get_parameter("symmetries"))
-#         calc.store_all()
-#         return calc
-#
-#     @Workflow.step
-#     def symmetrize(self):
-#         calc = self.get_step_calculations(self.previous_step)[0]
-#         tbmodel_file = calc.out.tb_model
-#         self.append_to_report("Symmetrizing tight-binding model...")
-#         self.attach_calculation(self.run_symmetrize(tbmodel_file))
-#         self.next(self.get_next_step())
-#
-#     @Workflow.step
-#     def finalize(self):
-#         calc = self.get_step_calculations(self.previous_step)[0]
-#         self.add_result('tb_model', calc.out.tb_model)
-#         self.append_to_report('Added final tb_model to results.')
-#         self.next(self.exit)
+    @Workflow.step
+    def bandeval(self):
+        extraction_wf = self.get_step_workflows(self.start)[0]
+        band_params = self.inherited_parameters(BandevaluationWorkflow)
+        # set the tight-binding model from the extraction
+        tb_model = extraction_wf.out.tb_model
+        band_params['tb_model'] = tb_model
+        self.add_result('tb_model', tb_model)
+        wf = BandevaluationWorkflow(params=band_params)
+        wf.store_all()
+        self.attach_workflow(wf)
+        self.next(self.finalize)
+
+    @Workflow.step
+    def finalize(self):
+        eval_wf = self.get_step_workflows(self.bandeval)[0]
+        self.add_result('difference', eval_wf.res.difference)
+        self.next(self.exit)
