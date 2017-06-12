147d770cd182676623205703108143b32b23346f
Dominik Gresch <greschd@gmx.ch>
2017-06-12 11:31:14 +0200

    Add dependency to bandstructure-utils AiiDA plugin

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
         reentry_register=True,
         install_requires=[
             'aiida-core',
+            'aiida-bandstructure-utils'
         ],
         extras_require={
         },
