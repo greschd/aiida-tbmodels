7bb4de6bf39732054d7e5a73e379a4dad16db600
Dominik Gresch <greschd@gmx.ch>
2017-07-07 12:24:14 +0200

    Add test for symmetrize

---
+++ tests/test_symmetrize.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, unicode_literals
+
+def test_symmetrize(configure_with_daemon, sample, get_tbmodels_process_inputs):
+    from aiida.orm import DataFactory
+    from aiida.work.run import run
+
+    process, inputs = get_tbmodels_process_inputs('tbmodels.symmetrize')
+
+    SinglefileData = DataFactory('singlefile')
+
+    input_model = SinglefileData()
+    input_model.add_path(sample('model.hdf5'))
+    inputs.tb_model = input_model
+
+    input_symmetries = SinglefileData()
+    input_symmetries.add_path(sample('symmetries.hdf5'))
+    inputs.symmetries = input_symmetries
+
+    output = run(process, **inputs)
+    assert isinstance(output['tb_model'], SinglefileData)
