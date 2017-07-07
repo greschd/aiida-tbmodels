fe7a72b9048422e98fc20db14d183bf3745c6ee4
Dominik Gresch <greschd@gmx.ch>
2017-07-08 00:30:12 +0200

    Change to single import from aiida_pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os.path import abspath, join
-
 import pytest
 
-from aiida_pytest.configure import *
-from aiida_pytest.process import *
-
-@pytest.fixture
-def sample():
-    def inner(name):
-        return join(abspath('./samples'), name)
-    return inner
+from aiida_pytest import * 
 
 @pytest.fixture
 def get_tbmodels_process_inputs(get_process_inputs):
