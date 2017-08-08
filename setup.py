676c04e67276bb5982274118b9b356029e691388
Dominik Gresch <greschd@gmx.ch>
2017-08-08 11:10:22 +0200

    Change license to MIT

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ if __name__ == '__main__':
         description='AiiDA Plugin for running TBmodels',
         author='Dominik Gresch',
         author_email='greschd@gmx.ch',
-        license='GPL',
+        license='MIT',
         classifiers=[
             'Development Status :: 3 - Alpha',
             'Environment :: Plugins',
             'Framework :: AiiDA',
             'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'License :: OSI Approved :: MIT License',
             'Programming Language :: Python :: 2.7',
             'Topic :: Scientific/Engineering :: Physics'
         ],
