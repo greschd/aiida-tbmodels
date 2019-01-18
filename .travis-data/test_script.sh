d8dc03fe28c0be237a33c287ec3eb3f4199d87f8
Dominik Gresch <greschd@gmx.ch>
2019-01-18 20:49:15 +0100

    Add print-status flag on Travis run.

--- .travis-data/test_script.sh
+++ .travis-data/test_script.sh
@@ -8,7 +8,7 @@ case "$TEST_TYPE" in
         # Run the AiiDA tests
         python ${TRAVIS_BUILD_DIR}/.travis-data/configure.py ${TRAVIS_BUILD_DIR}/.travis-data/test_config.yml ${TRAVIS_BUILD_DIR}/tests/config.yml;
         export AIIDA_PATH="${TRAVIS_BUILD_DIR}/tests"
-        cd ${TRAVIS_BUILD_DIR}/tests; py.test --quiet-wipe
+        cd ${TRAVIS_BUILD_DIR}/tests; py.test --quiet-wipe --print-status
         ;;
     pre-commit)
         pre-commit run --all-files || git status --short && git diff
