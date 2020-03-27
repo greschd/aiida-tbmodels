1a2a5682cb3126db4182211f95e62cfd08633be0
Dominik Gresch <a-dogres@microsoft.com>
2020-03-27 14:09:00 +0100

    Fix pre-commit errors

--- tests/test_slice.py
+++ tests/test_slice.py
@@ -7,6 +7,9 @@
 Defines tests for the tbmodels.slice calculation.
 """
 
+from aiida import orm
+from aiida.engine import run_get_node
+
 
 def test_slice(
     configure_with_daemon,  # pylint: disable=unused-argument
@@ -18,17 +21,12 @@ def test_slice(
     Run the tbmodels.slice calculation and check that it outputs
     a tight-binding model.
     """
-    from aiida.plugins import DataFactory
-    from aiida.orm import List
-    from aiida.engine import run_get_node
 
     builder = get_tbmodels_process_builder('tbmodels.slice')
 
-    SinglefileData = DataFactory('singlefile')  # pylint: disable=invalid-name
-    builder.tb_model = SinglefileData(file=sample('model.hdf5'))
-
-    builder.slice_idx = List(list=[0, 3, 2, 1])
+    builder.tb_model = orm.SinglefileData(file=sample('model.hdf5'))
+    builder.slice_idx = orm.List(list=[0, 3, 2, 1])
 
     output, calc = run_get_node(builder)
     check_calc_ok(calc)
-    assert isinstance(output['tb_model'], SinglefileData)
+    assert isinstance(output['tb_model'], orm.SinglefileData)
